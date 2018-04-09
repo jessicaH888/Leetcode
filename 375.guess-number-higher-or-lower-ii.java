d13f4998a5b249c34c7cebfd89c6faa44707da31
jessicaH888 <38018854+jessicaH888@users.noreply.github.com>
2018-04-09 15:35:59 -0700

    update 375.guess-number-higher-or-lower-ii.java

---
+++ 375.guess-number-higher-or-lower-ii.java
@@ -0,0 +1,57 @@
+/*
+ * [375] Guess Number Higher or Lower II
+ *
+ * https://leetcode.com/problems/guess-number-higher-or-lower-ii/description/
+ *
+ * algorithms
+ * Medium (36.08%)
+ * Total Accepted:    30K
+ * Total Submissions: 83.1K
+ * Testcase Example:  '1'
+ *
+ * We are playing the Guess Game. The game is as follows: 
+ * 
+ * I pick a number from 1 to n. You have to guess which number I picked.
+ * 
+ * Every time you guess wrong, I'll tell you whether the number I picked is
+ * higher or lower. 
+ * 
+ * However, when you guess a particular number x,  and you guess wrong, you pay
+ * $x. You win the game when you guess the number I picked.
+ * 
+ * 
+ * Example:
+ * 
+ * n = 10, I pick 8.
+ * 
+ * First round:  You guess 5, I tell you that it's higher. You pay $5.
+ * Second round: You guess 7, I tell you that it's higher. You pay $7.
+ * Third round:  You guess 9, I tell you that it's lower. You pay $9.
+ * 
+ * Game over. 8 is the number I picked.
+ * 
+ * You end up paying $5 + $7 + $9 = $21.
+ * 
+ * 
+ * 
+ * Given a particular n ≥ 1, find out how much money you need to have to
+ * guarantee a win.
+ * 
+ * Credits:Special thanks to @agave and @StefanPochmann for adding this problem
+ * and creating all test cases.
+ */
+class Solution {
+    public int getMoneyAmount(int n) {
+        int[][] dp = new int[n + 1][n + 1];
+        for(int i = n - 1; i >= 1; i--) {
+        	for(int j = i + 1; j <= n; j++) {
+        		int min = Integer.MAX_VALUE;
+        		for(int k = i; k < j; k++) {
+        			min = Math.min(min, k + Math.max(dp[i][k - 1], dp[k + 1][j]));
+        		}
+        		dp[i][j] = min;
+        	}
+        }
+        return dp[1][n];
+    }
+}
